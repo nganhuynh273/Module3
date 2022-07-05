9fd528b6b4ee684577b1e19a610bd66a960c628a
nganhuynh273 <huynhphuongngan273@gmail.com>
2022-07-05 08:33:58 +0700

    [Thực hành] Truy vấn dữ liệu với CSDL Quản lý sinh viên

--- Database Design/insert-into.sql
+++ Database Design/insert-into.sql
@@ -7,12 +7,14 @@ VALUES (2, 'A3', '2008-12-20', 1);
 INSERT INTO Class
 VALUES (3, 'B3', current_date, 0);
 
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO Student (StudentName, Adress, Phone, Status, ClassId)
 VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
-INSERT INTO Student (StudentName, Address, Status, ClassId)
+INSERT INTO Student (StudentName, Adress, Status, ClassId)
 VALUES ('Hoa', 'Hai phong', 1, 1);
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO Student (StudentName, Adress, Phone, Status, ClassId)
 VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+
+
 
 INSERT INTO Subject
 VALUES (1, 'CF', 5, 1),
